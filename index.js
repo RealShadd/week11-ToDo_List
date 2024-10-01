aca11fa8c03cc3bd4bba7f22b7f949ab7578241c
RealShadd <tsyganova.eu@gmail.com>
2024-10-01 22:18:13 +0300

    check-task-commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 const addButton = document.getElementById("addButton");
-addButton.addEventListener("click", function () {
+function createTask() {
   const taskText = taskInput.value;
   const newTask = document.createElement("li");
   newTask.textContent = taskInput.value;
   taskList.append(newTask);
   taskInput.value = "";
-});
+}
+function checkTask(event) {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("completed");
+  }
+}
+
+addButton.addEventListener("click", createTask);
+taskList.addEventListener("click", checkTask);
